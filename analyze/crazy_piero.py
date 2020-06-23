dbeb8f582f08e702760019b5aec08ae9be7526ba
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-06-23 17:59:34 +0900

    Update, saikoro game.

--- analyze/crazy_piero.py
+++ analyze/crazy_piero.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
-import random, os, sys
+import random
+import os
+import sys
+
 
 class Dice:
     def __init__(self):
-      self.saikoro = ["⚀","⚁","⚂","⚃","⚄","⚅"]
-      self.num = random.randint(0,1)
+        self.saikoro = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
+        self.num = random.randint(0, 1)
+
 
 eyes = Dice().saikoro
 ice = Dice().num
 
 if(ice == 0):
-  sin = ' '.join([random.choice(eyes) for i in range(5)])
-  print("Dice shuffle 5 times [心] ... " + sin)
-elif(ice == 1): 
-  gi = ' '.join([random.choice(eyes) for i in range(5)])
-  print("Dice shuffle 5 times [義] ... " + gi)
+    sin = ' '.join([random.choice(eyes) for i in range(5)])
+    print("Dice shuffle 5 times [心] ... " + sin)
+elif(ice == 1):
+    gi = ' '.join([random.choice(eyes) for i in range(5)])
+    print("Dice shuffle 5 times [義] ... " + gi)
 else:
-  print('Error? ... not use.')
-
+    print('Error? ... not use.')
