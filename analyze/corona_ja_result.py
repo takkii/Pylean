522a14081bd848322dee82d02ace98439561aa0f
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-08-27 17:01:29 +0900

    Update, new instance.

--- analyze/corona_ja_result.py
+++ analyze/corona_ja_result.py
@@ -2,10 +2,14 @@ import re
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
-session = HTMLSession()
-response = session.get('https://corona-stats.online/Japan')
-soup = BeautifulSoup(response.text,"lxml")
-[s.decompose() for s in soup('style')]
-pat = re.compile(r"<[^>]*?>")
-corona_ja_result = (pat.sub("", soup.text))
-print(corona_ja_result)
+class corona_ja_result:
+    def __init__(self):
+      session = HTMLSession()
+      response = session.get('https://corona-stats.online/Japan')
+      soup = BeautifulSoup(response.text,"lxml")
+      [s.decompose() for s in soup('style')]
+      pat = re.compile(r"<[^>]*?>")
+      corona_ja_result = (pat.sub("", soup.text))
+      print(corona_ja_result)
+
+corona_ja_result()
