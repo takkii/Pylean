e3d2cb26f47b2b0b88b7f9395a5d01fca192fadf
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-08-28 18:46:09 +0900

    Update, version.py is pip upgrade installer.

--- analyze/version.py
+++ analyze/version.py
@@ -1,4 +1,21 @@
+import traceback
+import subprocess
 import sys
+import importlib
+import site
+import platform
 
-print(sys.version)
-print(sys.version_info)
+class InstallerClass:
+    py_update = ['python', '-m', 'pip', 'install', '--upgrade', 'pip']
+
+    def py_up_method(self):
+        try:
+            py_update_win = subprocess.run(self.py_update, encoding='utf-8', stderr=subprocess.PIPE)
+            print(py_update_win)
+            print(sys.version)
+            print(sys.version_info)
+        except Exception:
+            traceback.print_exc()
+
+InstClass = InstallerClass()
+InstClass.py_up_method()
