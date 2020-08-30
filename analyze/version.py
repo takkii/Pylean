29d4629659888e6cb626a0f217368a56b54d7891
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-08-30 13:03:13 +0900

    Update, version.

--- analyze/version.py
+++ analyze/version.py
@@ -9,6 +9,7 @@ import traceback
 class InstallerClass(threading.Thread):
     py_setuptool = ['python', '-m', 'pip', 'install', '-U', 'pip', 'setuptools']
     py_update = ['python', '-m', 'pip', 'install', '--upgrade', 'pip']
+    py_mat = ['python', '-m', 'pip', 'install', 'matplotlib']
 
     def __init__(self):
         threading.Thread.__init__(self)
@@ -17,8 +18,10 @@ class InstallerClass(threading.Thread):
         try:
             py_update_win = subprocess.run(self.py_update, encoding='utf-8', stderr=subprocess.PIPE)
             py_setup_win= subprocess.run(self.py_setuptool, encoding='utf-8', stderr=subprocess.PIPE)
+            py_mat_win= subprocess.run(self.py_mat, encoding='utf-8', stderr=subprocess.PIPE)
             print(py_update_win)
             print(py_setup_win)
+            print(py_mat_win)
             print(sys.version)
 
         except Exception:
