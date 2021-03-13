91188d95d1dd8b969497cccac363b1a6cffb1372
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-03-13 13:10:14 +0900

    Update, class and thread add.

--- analyze/hyakunin_load.py
+++ analyze/hyakunin_load.py
@@ -1,14 +1,26 @@
 import sys
+import threading
 
-file_load = open('../input/hyakunin.txt', 'r', encoding='UTF-8')
-data = file_load.readlines()
-file_load.close()
 
-line: str
-for line in data:
-    arg_sys = sys.argv
-    match_word = arg_sys[1]
-    if line.find(match_word) < 0:
-        continue
-    output = line[:-1]
-    print(output)
+class Chihayahuru(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        file_load = open('../input/hyakunin.txt', 'r', encoding='UTF-8')
+        data = file_load.readlines()
+        file_load.close()
+
+        line: str
+        for line in data:
+            arg_sys = sys.argv
+            match_word = arg_sys[1]
+            if line.find(match_word) < 0:
+                continue
+            output = line[:-1]
+            print(output)
+
+
+thread = Chihayahuru()
+thread.run()
