04e3095ae7cfab7c431d5bca476c8acd33d40d8a
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-03-20 11:11:52 +0900

    Update.

--- analyze/corona_ja_result_gui.py
+++ analyze/corona_ja_result_gui.py
@@ -1,10 +1,22 @@
+import os
+import threading
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Coronavirus Tracker")
-root.geometry("1063x464")
-corosuke = tk.PhotoImage(file="../images/corona_image.gif")
-canvas = tk.Canvas(bg="black", width=1063, height=464)
-canvas.place(x=0, y=0)
-canvas.create_image(0, 0, image=corosuke, anchor=tk.NW)
-root.mainloop()
+class CoronaJaResult(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        cwd = os.path.expanduser("~")
+        root = tk.Tk()
+        root.title("Coronavirus Tracker")
+        root.geometry("1466x499")
+        corosuke = tk.PhotoImage(file=cwd + "/GitHub/Pylean/analyze/image/corona_effect.gif")
+        canvas = tk.Canvas(bg="black", width=1466, height=499)
+        canvas.place(x=0, y=0)
+        canvas.create_image(0, 0, image=corosuke, anchor=tk.NW)
+        root.mainloop()
+
+thread = CoronaJaResult()
+thread.run()
