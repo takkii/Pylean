54d8c197ea277be08bc6071ecada1a5a806dc25d
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-03-20 11:12:03 +0900

    Update.

--- analyze/rss.py
+++ analyze/rss.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
 session = HTMLSession()
-response = session.get('https://standb.herokuapp.com')
+response = session.get('https://takkii.github.io/')
 soup = BeautifulSoup(response.text, "lxml")
 [s.decompose() for s in soup('style')]
 pat = re.compile(r"<[^>]*?>")
